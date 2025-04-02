3b2640ee0de11d3fba6fb03cd2a661c4bc9ca0e4
gsilva222 <goncaloamesilva@gmail.com>
2025-04-02 13:19:56 +0100

    banana

---
+++ Pages/Shared/Mercado.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AtivoPlus.Pages
+{
+    public class MercadoModel : PageModel
+    {
+        public void OnGet()
+        {
+            // Aqui podes adicionar lógica para carregar dados, se quiseres
+        }
+    }
+}
